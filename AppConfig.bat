95642d7769f6001ba15afac5dcd246269f88d990
Ivan Rodrigues <ivan2g.rodriigues@gmail.com>
2024-06-30 11:08:25 -0300

    First commit

---
+++ AppConfig.bat
@@ -0,0 +1,18 @@
+rem winget list
+
+rem winget uninstall 
+
+rem App a instalar
+rem Terminal, Vim, VLC, 7zip Git, Steam
+rem Notion, Adobe Acrobat
+winget install Microsoft.WindowsTerminal
+winget install Neovim.Neovim
+winget install VideoLAN.VLC
+winget install 7zip.7zip
+winget install Git.Git
+winget install Valve.Steam
+winget install Notion.Notion
+winget install XPDP273C0XHQH2OBS
+winget install Project.OBSStudio
+
+rem winget install 
---
+++ Shutdown/google.bat
@@ -0,0 +1,3 @@
+start C:\"Program Files"\Google\Chrome\Application\chrome.exe
+timeout 200
+shutdown /t 0 /s /f
